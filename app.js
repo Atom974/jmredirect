ffe511995bfa54cff84900885674df4ad1460d18
ATom <tom.apostin@protonmail.com>
2021-11-08 17:38:52 +0400

    [perf]modif fonctionnement css

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const path = require('path')
 const app = express()
 const port = 3000;
 const bodyParser = require('body-parser')
+var favicon = require('serve-favicon');
+
+
+//app.use(favicon(path.join(__dirname,'public','index.png')));
 
 app.use(bodyParser.urlencoded({ extended: false }))
  
@@ -15,7 +19,7 @@ app.get('/', (req, res) => {
 	res.render('index')
 })
 app.post('/', (req, res) => {
-	if(req.body.departement == 974)
+	if(req.body.departement.startsWith('974') && req.body.departement.lenght > 3 && req.body.departement.lenght < 5)
 		res.redirect('https://www.jmstore.re/');
 	else
 		res.render('wrong')
